2a8eb8c995810f19fe58f70bf38a17af5a6b21f9
Rina <60498923+maksmaxis@users.noreply.github.com>
2022-11-05 20:41:14 +0600

    Update stage-1-kernel-update.sh

--- packer/scripts/stage-1-kernel-update.sh
+++ packer/scripts/stage-1-kernel-update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Install elrepo
-yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+sudo yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 # Install new kernel
-yum --enablerepo elrepo-kernel install kernel-ml -y
-# Remove older kernels (Only for demo! Not Production!)
-rm -f /boot/*3.10*
+sudo yum --enablerepo elrepo-kernel install kernel-ml -y
+
 # Update GRUB
-grub2-mkconfig -o /boot/grub2/grub.cfg
-grub2-set-default 0
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-set-default 0
 echo "Grub update done."
 # Reboot VM
-shutdown -r now
+sudo shutdown -r now
